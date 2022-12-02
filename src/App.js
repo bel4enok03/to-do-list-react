f99ee8edaf133b458d0850febb9d22ec747779bf
bel4enok03 <belskaya-tanyush@mail.ru>
2022-12-02 23:32:00 +0400

    *

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import imageTwo from './image/shopping.webp';
+import image from './image/man.webp';
+import { GroceryList } from './GroceeryList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<img src={imageTwo} width='150px' alt='shopping' />
+			<div className='container'>
+				<h1>Grocery list</h1>
+			</div>
+			<GroceryList />
+			<div className='container'>
+				<img src={image} width='150px' alt='man' />
+			</div>
+		</div>
+	);
 }
 
 export default App;
